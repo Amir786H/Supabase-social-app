c61468b48bd771f19018d39f8bc4685860c71e8c
amir786h <amirhusain3737@gmail.com>
2024-07-31 22:18:02 +0530

    Supabase added

--- app/signUp.jsx
+++ app/signUp.jsx
@@ -9,6 +9,7 @@ import { useRouter } from "expo-router";
 import { wp, hp } from "../helpers/common";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { supabase } from "../lib/supabase";
 
 const SignUp = () => {
   const router = useRouter();
@@ -17,10 +18,28 @@ const SignUp = () => {
   const passwordRef = useRef("");
   const [loading, setLoading] = useState(false);
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     if(!emailRef.current || !passwordRef.current){
       Alert.alert('SignUp', 'Please fill all the fields');
       return;
+    }
+
+    let name = nameRef.current.trim();
+    let email = emailRef.current.trim();
+    let password = passwordRef.current.trim();
+
+    setLoading(true);
+
+    const {data: {session}, error} = await supabase.auth.signUp({
+      email,
+      password
+    });
+    setLoading(false);
+    
+    console.log('session:', session);
+    console.log('error:', error);
+    if(error) {
+      Alert.alert('Sign up', error.message);
     }
   };
 

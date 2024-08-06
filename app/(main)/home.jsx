bc598320449993d6c9bb996935ac7bfae07ddd57
amir786h <amirhusain3737@gmail.com>
2024-08-06 10:38:00 +0530

    Expo image added and other compon

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,13 +1,19 @@
-import { StyleSheet, Text, Button, View, Alert } from "react-native";
+import { StyleSheet, Text, Button, View, Alert, Pressable } from "react-native";
 import React from "react";
 import ScreenWrapper from "../../components/ScreenWrapper";
 import { useAuth } from "../../contexts/AuthContext";
 import { supabase } from "../../lib/supabase";
+import { theme } from "../../constants/theme";
+import { hp, wp } from "../../helpers/common";
+import Icon from "../../assets/icons";
+import { useRouter } from "expo-router";
+import Avatar from "../../components/Avatar";
 
 const Home = () => {
+  const router = useRouter();
   const { user, setAuth } = useAuth();
 
-  console.log('user: ', user);
+  console.log("user: ", user);
 
   const onLogout = async () => {
     // setAuth(null);
@@ -18,8 +24,40 @@ const Home = () => {
   };
 
   return (
-    <ScreenWrapper>
-      <Text>Home</Text>
+    <ScreenWrapper bg="white">
+      <View style={styles.container}>
+        {/* header */}
+        <View style={styles.header}>
+          <Text style={styles.title}>HookUp</Text>
+          <View style={styles.icons}>
+            <Pressable onPress={() => router.push("notifications")}>
+              <Icon
+                name="heart"
+                size={hp(3.2)}
+                strokeWidth={2}
+                color={theme.colors.text}
+              />
+            </Pressable>
+            <Pressable onPress={() => router.push("newPost")}>
+              <Icon
+                name="plus"
+                size={hp(3.2)}
+                strokeWidth={2}
+                color={theme.colors.text}
+              />
+            </Pressable>
+            <Pressable onPress={() => router.push("profile")}>
+              {/* <Icon name="user" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} /> */}
+              <Avatar
+                uri={user?.image}
+                size={hp(4.3)}
+                rounded={theme.radius.sm}
+                style={{ borderWidth: 2 }}
+              />
+            </Pressable>
+          </View>
+        </View>
+      </View>
       <Button title="logout" onPress={onLogout} />
     </ScreenWrapper>
   );
@@ -27,4 +65,59 @@ const Home = () => {
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    marginHorizontal: wp(4),
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(3.2),
+    fontWeight: theme.fonts.bold,
+  },
+  avatarImage: {
+    height: hp(4.3),
+    width: hp(4.3),
+    borderRadius: theme.radius.sm,
+    borderCurve: "continuous",
+    borderColor: theme.colors.gray,
+    borderWidth: 3,
+  },
+  icons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 18,
+  },
+  pillText: {
+    color: "white",
+    fontSize: hp(1.2),
+    fontWeight: theme.fonts.bold,
+  },
+  pill: {
+    position: "absolute",
+    right: -10,
+    top: -4,
+    height: hp(2.2),
+    width: hp(2.2),
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colors.roseLight,
+    borderRadius: 20,
+  },
+  noPosts: {
+    fontSize: hp(2),
+    textAlign: "center",
+    color: theme.colors.text,
+  },
+  listStyle: {
+    paddingTop: 20,
+    paddingHorizontal: wp(4),
+  },
+});
